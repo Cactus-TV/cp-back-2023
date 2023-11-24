49f017b2bf6fdfe91022489bc054a208628ff19d
Artem <boss.motyakin@mail.ru>
2023-11-25 01:36:57 +0300

    add ip

--- django_template/settings.py
+++ django_template/settings.py
@@ -85,6 +85,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://0.0.0.0:8000",
+    "http://158.160.129.203:8000"
 ]
 
 CORS_URLS_REGEX = r'^/back-bridge/.*$'
